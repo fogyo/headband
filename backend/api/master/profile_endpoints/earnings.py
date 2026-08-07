f3ad67c6de431698e674777c5b9bd1179ffcf2a8
fogyo <sitalovm@gmail.com>
2026-08-07 15:23:08 +0300

    v14

--- backend/api/master/profile_endpoints/earnings.py
+++ backend/api/master/profile_endpoints/earnings.py
@@ -8,8 +8,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from backend.database import get_db_session, miniapp_db_fcn
 from backend.database.responses import IDResponse, StatusResponse
-from backend.telegram_bot import bot
-from backend.telegram_bot.bot_main import handle_rating
+from backend.telegram_bot.bot_main import handle_rating, bot
 
 
 #Requests
